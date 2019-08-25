81e4a5c77ee8bc3a7a6f363b937c04c021874e3e
Syed Shaazib Tanvir <syedshaazib@gmail.com>
2019-08-25 08:50:19 +0530

    Added singleton script

---
+++ Project Files/Scripts/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+/*
+ *
+ * USAGE
+ * If there's a class Test.cs
+ * Replace Monobehaviour by Singleton<Test>
+ * If you want to access Test's data from another class you can do it globally Test.Instance.variablename
+ * If you want to write an awake function for test override it
+ * This script will become global
+ * 
+ */
+
+//T is the class we'll convert to singleton
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    //The instance of our class
+    public static T Instance { get; protected set; }
+
+    //It's protected so it you want to write awake code in the class just override it
+    protected virtual void Awake()
+    {
+        //If instance doesn't exist
+        if (Instance == null)
+        {
+            //Set the instance
+            Instance = GetComponent<T>();
+            //Make it not destroy in scene change
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            //Destroy any additional crap
+            Destroy(gameObject);
+        }
+    }
+}
