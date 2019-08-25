a14e7cbabbd2a879fd2fbf5b7a0faa4d4c77c43e
GameMaker46 <38005728+GameMaker46@users.noreply.github.com>
2019-08-25 20:33:57 +0530

    Updated movement
    *USAGE*
    Add a new layer mask Platforms and make all the platforms of that mask
    also change the platform layer in the inspector of movement

--- File Submission/Movement/Movement.cs
+++ File Submission/Movement/Movement.cs
@@ -1,51 +1,81 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 //The movement of player
 [RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
-    /// <summary>
-    /// The rigidbody
-    /// </summary>
+    //The rigidbody
     private Rigidbody2D rb;
+    //Some bools
+    private bool doubleJump, canJump;
+    //The collider to check ground
+    private BoxCollider2D boxCollider;
+    //The jump count
+    private int jumpCount;
+
+    //The platorm layer be sure to create it in layers and set platforms to layer
+    [SerializeField] LayerMask platforms;
 
     /// <summary>
-    /// The awake method
+    /// Awake method
     /// </summary>
     private void Awake()
     {
-        //The rigidbody init
+        //Set the values
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        rb.drag = 5f;
+        rb.gravityScale = 30f;
     }
 
     /// <summary>
-    /// Gets input
-    /// </summary>
-    /// <returns>vector2</returns>
-    private Vector2 GetInput()
-    {
-        //The input axis
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        //The value
-        return new Vector2(horizontal, vertical);
-    }
-
-    /// <summary>
-    /// Fixed Update
+    /// Fixed Update method
     /// </summary>
     private void FixedUpdate()
     {
-        //Check if player can move
+        //Check if player can move in the first place
         if (GameHandler.Instance.canMove)
         {
-            //The destination
-            Vector3 destination = GetInput();
-            destination = destination.normalized * Time.fixedDeltaTime * GameHandler.Instance.speed;
+            //Ground check
+            GroundCheck();
 
-            //Move the player
-            rb.MovePosition(destination + transform.position);
+            //Get the position to go to
+            Vector3 destination = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            destination = destination.normalized * GameHandler.Instance.speed * Time.fixedDeltaTime;
+
+            //Move horizontaly
+            rb.MovePosition(transform.position + destination);
+
+            //Jump
+            if (canJump)
+            {
+                jumpCount = 0;
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    rb.AddForce(Vector2.up * GameHandler.Instance.jumpForce, ForceMode2D.Impulse);
+                    jumpCount++;
+                    return;
+                }
+            }
+
+            //Double jump
+            if (Input.GetKeyDown(KeyCode.Space) && jumpCount <= 1)
+            {
+                rb.AddForce(Vector2.up * GameHandler.Instance.jumpForce, ForceMode2D.Impulse);
+                jumpCount++;
+            }
         }
+    }
+
+    /// <summary>
+    /// Grounded check
+    /// </summary>
+    private void GroundCheck()
+    {
+        //raycast
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, 0.1f, platforms);
+        canJump = hit.collider != null;
     }
 }
