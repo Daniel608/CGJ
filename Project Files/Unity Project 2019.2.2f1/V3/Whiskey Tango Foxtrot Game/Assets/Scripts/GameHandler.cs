27d62c90b22454547b809a85445e45059661b7f8
Daniel <43499487+Daniel@users.noreply.github.com>
2019-08-26 22:00:57 +1000

    V3

---
+++ Project Files/Unity Project 2019.2.2f1/V3/Whiskey Tango Foxtrot Game/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//The base class for game progression
+public class GameHandler : Singleton<GameHandler>
+{
+    //The speed
+    public float speed;
+
+    //Check if it can move
+    public bool canMove = true;
+
+    #region TitleScreen
+
+    //The first scene's name
+    public string LevelSceneName;
+
+    /// <summary>
+    /// Plays the game
+    /// </summary>
+    public void Play()
+    {
+        //Load the first scene
+        SceneManager.LoadScene(LevelSceneName);
+    }
+
+    /// <summary>
+    /// Exits the game
+    /// </summary>
+    public void Exit()
+    {
+        //Quit the game
+        Application.Quit();
+        Debug.Log("Quitting...");
+    }
+
+    #endregion
+}
