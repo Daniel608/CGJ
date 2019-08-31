a1e0661ed3d00f28c6ea890a41edc395760362db
Daniel <43499487+Daniel@users.noreply.github.com>
2019-08-31 21:48:46 +1000

    V7

---
+++ Project Files/Unity Project 2019.2.2f1/V7/Whiskey Tango Foxtrot Game/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* USAGE
+ *
+ * MAKE A TEXT CALLED "Power Text"
+ *
+ * CHANGE THE JUMP BOOST AND SPEED BOOST AND TIME TO SEE IT
+ * 
+ */
+public class PowerUp : MonoBehaviour
+{
+    public float jumpBoost, speedBoost, timeForBoost = 10f;
+
+    private GameObject powerText;
+    private bool poweredUp;
+    private float previousJump, previousSpeed;
+
+    private void Awake()
+    {
+        powerText = GameObject.Find("Power Text");
+
+        powerText.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && Vector2.Distance(transform.position,
+            GameHandler.Instance.player.transform.position) <= 2f && !poweredUp)
+        {
+            Movement movement = GameHandler.Instance.player.GetComponent<Movement>();
+            previousJump = movement.jump;
+            previousSpeed = movement.speed;
+            movement.jump += jumpBoost;
+            movement.speed += speedBoost;
+            powerText.SetActive(true);
+            StartCoroutine(Disappear(movement));
+            poweredUp = true;
+        }
+    }
+
+    IEnumerator Disappear(Movement movement)
+    {
+        yield return new WaitForSeconds(timeForBoost);
+        powerText.SetActive(false);
+        movement.speed = previousSpeed;
+        movement.jump = previousJump;
+        Destroy(gameObject);
+    }
+}
---
+++ Project Files/Unity Project 2019.2.2f1/V7/Whiskey Tango Foxtrot Game/Assets/Scripts/simplepause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class simplepause : MonoBehaviour
+{
+    public bool isenabled;
+    public GameObject pausePanel;
+
+    void Start()
+    {
+        isenabled = false;
+        pausePanel.SetActive(false);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isenabled)
+            {
+                pausePanel.SetActive(false);
+                isenabled = false;
+            } else
+            {
+                pausePanel.SetActive(true);
+                isenabled = true;
+            }
+        }
+    }
+}
